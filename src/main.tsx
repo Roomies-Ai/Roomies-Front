91d47c0a8c4bdb099b6d6877db00a74a0ef5140c
Osher <osherzohar80@gmail.com>
2026-01-25 22:13:00 +0200

    Revert "initial google auth"
    This reverts commit 7e7e445d54f0ab4c602ad04e42cc1dea035bc728.

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css'
 import App from './App.tsx'
-import GoogleAuth from './components/auth/GoogleAuth.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <GoogleOAuthProvider clientId="279167213238-jl0r6dtbvhuesqh2kjv12rg87o870vsc.apps.googleusercontent.com">
-        <GoogleAuth />
-      </GoogleOAuthProvider>
+      <App />
     </BrowserRouter>
   </StrictMode>,
 )
