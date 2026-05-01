7e4059e9a94c5294ee37d603b4a39ad7fe86db72
Assaf Revacha <assaf.git.arm@gmail.com>
2026-05-01 17:31:14 +0300

    add token

--- src/api/Axios.ts
+++ src/api/Axios.ts
@@ -5,6 +5,15 @@ const apiClient = axios.create({
     headers: {
         'Content-Type': 'application/json',
     },
+    withCredentials: true,
+});
+
+apiClient.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 });
 
 export default apiClient;
