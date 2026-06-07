fe54d89dc406249d3f8f99f90a529d9ccab2caf3
Osher <osherzohar80@gmail.com>
2026-06-07 14:23:38 +0300

    add rtk to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
 import notificationsReducer from './slices/notificationsSlice';
+import { api } from '../api/api';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     notifications: notificationsReducer,
+    [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
-    }),
+    }).concat(api.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
