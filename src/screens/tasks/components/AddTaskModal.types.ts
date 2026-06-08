e3dc6c6b2587d21697b60e3d7540684d54ca6b79
AmitMizrahii <miamit112@gmail.com>
2026-06-08 07:32:44 +0300

    updated useTask

--- src/screens/tasks/components/AddTaskModal.types.ts
+++ src/screens/tasks/components/AddTaskModal.types.ts
@@ -1,4 +1,4 @@
-import type { Task } from '../types/tasks.types';
+import type { Task, RecurrenceRule } from '../types/tasks.types';
 
 export interface AddTaskModalProps {
     isOpen: boolean;
@@ -7,6 +7,7 @@ export interface AddTaskModalProps {
     onAdd: (householdId: string, taskData: any) => void;
     onUpdate?: (taskId: string, updates: any) => void;
     onDelete?: (taskId: string) => void;
+    onClearRecurrence?: (taskId: string) => void;
     onRefresh?: () => void;
     taskToEdit?: Task;
     lockedHouseholdId?: string;
